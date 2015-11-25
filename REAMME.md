cdff76e21c863a67e7d3ba53a4af33e5ec02db36
Haris Mohammad <hmohammad@spartaglobal.com>
2015-11-25 17:41:59 +0000

    Tumblr Automation with Watir

---
+++ REAMME.md
@@ -0,0 +1,4 @@
+#Watir-Selenium
+
+##Decription:
+###This project demonstrates Watir-Api used to automate Tumblr login from YML file\n As well as posting a blog and verfiying it after
---
+++ tumblr.rb
@@ -0,0 +1,36 @@
+require 'watir-webdriver'
+require 'yaml'
+
+browser = Watir::Browser.new :firefox
+browser.goto "http://tumblr.com/login"
+
+email = ''
+password = ''
+
+data = YAML.load(File.open('form.yml'))
+ data['credentials'].each do |yaml|
+ email = yaml['email']
+ password = yaml["password"]
+ end
+browser.text_field(name: 'user[email]').set("#{email}")
+browser.text_field(name: 'user[password]').set("#{password}")
+browser.button(id: 'signup_forms_submit').click
+
+browser.element(class: 'icon_post_text').click
+text =browser.element(class: 'editor-plaintext')
+text.send_keys("mkscmld\tweklfwefkl")
+
+
+post_button = browser.button(class: "create_post_button").click
+
+post_body = browser.elements(class: "post_body")
+found = false
+post_body.each do |o| 
+   if (o.text).match /weklfwefkl/
+	puts "Found! \n Your post is: #{o.text}" 
+	found = true 
+	end
+end
+print "not found" if found == false
+
+browser.close
